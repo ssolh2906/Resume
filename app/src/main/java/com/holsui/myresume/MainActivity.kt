e2b608f864b6673548c77002231d4540c175cba8
Solhee Tucker <solheepark29@gmail.comsolheepark29@gmail.comsolheepark29@gmail.com>
2023-11-02 18:44:17 -0700

    get info for capturing

--- app/src/main/java/com/holsui/myresume/MainActivity.kt
+++ app/src/main/java/com/holsui/myresume/MainActivity.kt
@@ -1,11 +1,9 @@
 package com.holsui.myresume
 
-import android.content.pm.PackageManager
+import android.graphics.Rect
 import android.os.Bundle
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
@@ -18,9 +16,17 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.LayoutCoordinates
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.layout.onSizeChanged
+import androidx.compose.ui.layout.positionInWindow
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.IntSize
 
 class MainActivity : ComponentActivity() {
 
@@ -36,9 +42,17 @@ class MainActivity : ComponentActivity() {
     @Composable
     @OptIn(ExperimentalMaterial3Api::class)
     private fun ResumeScreen() {
+        var rectSize by remember { mutableStateOf(IntSize(0, 0)) }
+        var captureRect: Rect? by remember { mutableStateOf(null) }
+
         Surface(
             modifier = Modifier.fillMaxSize()
-                .aspectRatio(8.5f/11f),
+                .aspectRatio(8.5f/11f)
+                .onSizeChanged { size ->
+                    rectSize = size}
+                .onGloballyPositioned {
+                    captureRect = getAbsoluteRect(layoutCoordinate = it, rectSize = rectSize)
+                },
         ) {
             Scaffold(
                 topBar = {
@@ -60,5 +74,17 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+    }
+
+
+    private fun getAbsoluteRect(
+        layoutCoordinate: LayoutCoordinates,
+        rectSize: IntSize,
+    ): Rect {
+        val offsetX = layoutCoordinate.positionInWindow().x
+        val offsetY = layoutCoordinate.positionInWindow().y
+        val top = offsetY.toInt()
+        val left = offsetX.toInt()
+        return Rect(left, top, left + rectSize.width, top + rectSize.height)
     }
 }
