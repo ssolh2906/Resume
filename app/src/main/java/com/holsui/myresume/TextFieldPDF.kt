9eb986ebc040197bcfb4833b2fcc5dd79e7a8d41
Solhee Tucker <solheepark29@gmail.comsolheepark29@gmail.comsolheepark29@gmail.com>
2023-11-13 16:10:59 -0800

    remove texts while taking screenshot

--- app/src/main/java/com/holsui/myresume/TextFieldPDF.kt
+++ app/src/main/java/com/holsui/myresume/TextFieldPDF.kt
@@ -1,11 +1,9 @@
 package com.holsui.myresume
 
-import android.util.Log
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.State
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -14,8 +12,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.onPlaced
 import androidx.compose.ui.layout.positionInRoot
-import androidx.compose.ui.layout.positionInWindow
-import androidx.compose.ui.text.PlatformTextStyle
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.sp
@@ -27,6 +23,7 @@ fun TextFieldPDF(
     modifier: Modifier = Modifier,
     defaultString: String = "",
     onTextPlaced: (String, TextInfo) -> Unit = { _, _ -> },
+    snapshotState: State<SnapshotState>,
     textStyle: TextStyle = TextStyle.Default
 ) {
     var currValue by remember { mutableStateOf(defaultString) }
@@ -47,12 +44,13 @@ fun TextFieldPDF(
                         y = coordinates.positionInRoot().y
                     )
                 )
-                Log.d("TF_PDF", "TextFieldPDF [P]: ${coordinates.positionInWindow()} ")
             },
         textStyle = textStyle.copy(
-            color = Color.Gray,
+            color = when(snapshotState.value) {
+                SnapshotState.STATE_READY -> Color.Transparent
+                else -> Color.Black
+            },
             fontSize = fontSize.sp,
-            platformStyle = PlatformTextStyle(includeFontPadding = false),
             textAlign = TextAlign.Center
         ),
     )
