058a36a91a00ab501b0034dfa2b88e209e983095
Solhee Tucker <solheepark29@gmail.comsolheepark29@gmail.comsolheepark29@gmail.com>
2023-11-13 16:12:02 -0800

    check font size

--- app/src/main/java/com/holsui/myresume/ResumeScreen.kt
+++ app/src/main/java/com/holsui/myresume/ResumeScreen.kt
@@ -5,7 +5,6 @@ import android.graphics.Bitmap
 import android.graphics.Rect
 import android.util.Log
 import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
@@ -14,6 +13,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ExitToApp
 import androidx.compose.material3.Button
@@ -37,17 +37,13 @@ import androidx.compose.ui.layout.LayoutCoordinates
 import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.layout.onSizeChanged
 import androidx.compose.ui.layout.positionInWindow
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import coil.compose.AsyncImage
-import coil.request.ImageRequest
 import com.holsui.myresume.ui.theme.SpacingInset
-import com.holsui.myresume.ui.theme.SpacingStack
 
 private const val LETTER_RATIO = 8.5f / 11f
 
@@ -117,12 +113,12 @@ fun ResumeScreen(
                             val tag = "name"
                             TextFieldPDF(
                                 tag = tag,
-                                defaultString = "SOLHEE PARK TUCKER",
-                                onTextPlaced = onTextPlaced,
-                                fontSize = 30,
                                 modifier = Modifier
-                                    .fillMaxWidth()
-                                    .padding(vertical = SpacingStack)
+                                    .fillMaxWidth(),
+                                defaultString = "HEADING",
+                                onTextPlaced = onTextPlaced,
+                                snapshotState = snapshotState,
+                                fontSize = 30
                             )
 
                         }
@@ -185,29 +181,25 @@ private fun LetterContents(
             .padding(SpacingInset)
     ) {
         Column(
-            verticalArrangement = Arrangement.SpaceBetween,
-            modifier = Modifier.fillMaxSize()
+            modifier = Modifier.fillMaxSize(),
         ) {
-            TextFieldPDF(
-                tag = "name",
-                defaultString = "SOLHEE TUCKER",
-                onTextPlaced = onTextPlaced,
-                fontSize = 50
-            )
-
-            Text(text = if (snapshotState.value == SnapshotState.STATE_READY) "뤠디ㅣㅣㅣ" else "낫레디",
-                modifier = Modifier.onGloballyPositioned {
-                })
-
-            AsyncImage(
-                model = ImageRequest.Builder(LocalContext.current)
-                    .data(
-                        "https://assets.pokemon.com/assets/cms2/img/pokedex/full/025.png",
+            Column(
+                modifier = Modifier
+                    .wrapContentHeight()
+                    .padding(vertical = 12.dp)
+                    .fillMaxWidth(),
+            ) {
+                for (i in 10..50 step 2) {
+                    TextFieldPDF(
+                        tag = i.toString(),
+                        fontSize = i,
+                        modifier = Modifier.wrapContentHeight(),
+                        defaultString = "ABCDEFDHIJKLMNO $i, ",
+                        onTextPlaced = onTextPlaced,
+                        snapshotState = snapshotState
                     )
-                    .crossfade(true)
-                    .build(),
-                contentDescription = null,
-            )
+                }
+            }
         }
     }
 }
