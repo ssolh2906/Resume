8b29481b6a9eb5871bf9176a388addbb0e39ad3f
Solhee Tucker <solheepark29@gmail.comsolheepark29@gmail.comsolheepark29@gmail.com>
2023-11-15 11:36:15 -0800

    Resume Contents UI

---
+++ app/src/main/java/com/holsui/myresume/ui/composables/ResumeCards.kt
@@ -0,0 +1,47 @@
+package com.holsui.myresume.ui.composables
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun ProjectCard(modifier: Modifier = Modifier) {
+    Surface(
+        modifier = modifier,
+        shape = RoundedCornerShape(12.dp),
+    ) {
+
+    }
+}
+
+@Composable
+fun FeaturedCard(modifier: Modifier = Modifier) {
+    Surface(
+        modifier = modifier,
+        shape = RoundedCornerShape(12.dp),
+    ) {
+
+    }
+}
+
+@Composable
+fun ExperienceCard(modifier: Modifier = Modifier) {
+    Surface(
+        modifier = modifier,
+        shape = RoundedCornerShape(12.dp),
+    ) {
+
+    }
+}
+
+@Composable
+fun EducationCard(modifier: Modifier = Modifier) {
+    Surface(
+        modifier = modifier,
+        shape = RoundedCornerShape(12.dp),
+    ) {
+
+    }
+}
--- app/src/main/java/com/holsui/myresume/ui/composables/SideBar.kt
+++ app/src/main/java/com/holsui/myresume/ui/composables/SideBar.kt
@@ -1,5 +1,7 @@
 package com.holsui.myresume.ui.composables
 
+import android.annotation.SuppressLint
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -8,16 +10,20 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Call
 import androidx.compose.material3.Divider
 import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.holsui.myresume.SnapshotState
 import com.holsui.myresume.TextFieldPDF
@@ -31,7 +37,7 @@ fun SideBar(
     modifier: Modifier = Modifier
 ) {
     Surface(
-        color = MaterialTheme.colorScheme.primaryContainer,
+        color = Color(0xFFF7F2FA),
         shape = RoundedCornerShape(
             topStart = 0.dp,
             topEnd = 16.dp,
@@ -40,11 +46,16 @@ fun SideBar(
         ),
         modifier = modifier
             .fillMaxHeight()
-            .width(300.dp)
     ) {
         Column {
             SideBarHeader("Contact", snapshotState, onTextPlaced)
-            SideBarItem("408-000-0001", snapshotState, onTextPlaced)
+            SideBarItem("408-000-0001", snapshotState, onTextPlaced) {
+                Icon(
+                    Icons.Default.Call,
+                    modifier = Modifier.size(24.dp),
+                    contentDescription = "Icon Call"
+                )
+            }
             SideBarItem("408-000-0000", snapshotState, onTextPlaced)
             Divider(modifier = Modifier.padding(horizontal = 16.dp), thickness = 1.dp)
         }
@@ -59,6 +70,7 @@ fun SideBarHeader(
 ) {
     Box(
         modifier = Modifier
+            .wrapContentHeight()
             .padding(vertical = 18.dp, horizontal = 16.dp)
     ) {
         TextFieldPDF(
@@ -75,13 +87,16 @@ fun SideBarHeader(
 fun SideBarItem(
     string: String,
     snapshotState: State<SnapshotState>,
-    onTextPlaced: (String, TextInfo) -> Unit
+    onTextPlaced: (String, TextInfo) -> Unit,
+    icon: @Composable (() -> Unit)? = null
 ) {
     Row(
         modifier = Modifier
             .padding(vertical = 18.dp, horizontal = 16.dp),
+        horizontalArrangement = Arrangement.Center,
+        verticalAlignment = Alignment.CenterVertically
     ) {
-        Icon(Icons.Default.Call, modifier = Modifier.size(24.dp), contentDescription = "Icon Call")
+        icon?.invoke()
         Spacer(modifier = Modifier.width(12.dp))
         TextFieldPDF(
             tag = string,
@@ -91,4 +106,14 @@ fun SideBarItem(
             onTextPlaced = onTextPlaced,
         )
     }
+}
+
+@SuppressLint("UnrememberedMutableState")
+@Composable
+@Preview
+fun PreviewSidebar() {
+    SideBar(snapshotState = mutableStateOf(SnapshotState.STATE_IDLE),
+        onTextPlaced = { _, _ ->
+
+        })
 }
