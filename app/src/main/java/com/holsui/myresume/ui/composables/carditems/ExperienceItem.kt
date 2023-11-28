e28adca01afab8cd0b0fcfbf577ff77dfaed9699
Solhee Tucker <solheepark29@gmail.comsolheepark29@gmail.comsolheepark29@gmail.com>
2023-11-28 11:48:48 -0800

    Experience Dot Items UI

--- app/src/main/java/com/holsui/myresume/ui/composables/carditems/ExperienceItem.kt
+++ app/src/main/java/com/holsui/myresume/ui/composables/carditems/ExperienceItem.kt
@@ -1,17 +1,24 @@
 package com.holsui.myresume.ui.composables.carditems
 
 import android.annotation.SuppressLint
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ExperimentalLayoutApi
 import androidx.compose.foundation.layout.FlowRow
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
@@ -20,6 +27,9 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Color.Companion.Gray
+import androidx.compose.ui.graphics.Color.Companion.Red
+import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.holsui.myresume.SnapshotState
@@ -76,6 +86,16 @@ fun ExperienceItem(
                         modifier = Modifier.padding(start = 4.dp, bottom = 4.dp)
                     )
                 }
+                if (dotItems != null) {
+                    for (item in dotItems) {
+                        DotItem(
+                            item = item,
+                            snapshotState = snapshotState,
+                            onTextPlaced = onTextPlaced
+                        )
+                    }
+                }
+                Spacer(modifier = Modifier.height(4.dp))
                 FlowRow {
                     if (checkBoxItems != null) {
                         for (item in checkBoxItems) {
@@ -89,6 +109,36 @@ fun ExperienceItem(
                 }
             }
         }
+    }
+}
+
+@Composable
+fun DotItem(
+    item: String,
+    snapshotState: State<SnapshotState>,
+    onTextPlaced: (String, TextInfo) -> Unit
+) {
+    Row(
+        horizontalArrangement = Arrangement.Start,
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier.padding(horizontal = 4.dp)
+    ) {
+        Surface(
+            shape = CircleShape,
+            color = Gray,
+            modifier = Modifier
+                .size(4.dp)
+                .offset(y = 1.dp)
+        ) {}
+        Spacer(modifier = Modifier.width(8.dp))
+        TextFieldPDF(
+            tag = "dot item $item",
+            fontSize = 10,
+            snapshotState = snapshotState,
+            modifier = Modifier.wrapContentSize(),
+            defaultString = item,
+            onTextPlaced = onTextPlaced
+        )
     }
 }
 
